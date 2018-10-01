4a610ef2cce2c9bd289bd3f29f95f1817b20c2a9
PtrMan <robertw89@googlemail.com>
2018-10-01 20:20:56 +0200

    Opt: Cythonized NarsMemory

--- code/python/nnesycef/UnifiedAgent.py
+++ code/python/nnesycef/UnifiedAgent.py
@@ -76,56 +76,13 @@ def genSdrByStringName(str_):
 	return genSdrByIntId(id_)
 
 
-# TODO< build acceleration structures for the symetric and asymetric acceleration >
-# memory which stores objects and retrieves them based on distance
-class NarsMemory(object):
-	def __init__(self):
-		self.objects = []
-
-		self.retrieveBeliefMinimalOverlap = 0.0 # 0.5
-
-	def appendObject(self, object_):
-		self.objects.append(object_)
-
-	# /param sdr the searched sdr by overlap
-	def retrieveBySdr(self, sdr):
-		# TODO TODO TODO TODO
-		# TODO< retieve from global storage >
-
-		# we retrieve from global storage by searching for the best fit with the minimal overlap
-
-		# TODO< search assymetric as done in ANSNA >
-
-		# TODO< we need a class with the 2 bit tuple acceleration for symetric retrival too >
-
-		if len(self.objects) == 0:
-			return None
-
-		bestObject = self.objects[0]
-		bestObjectOverlap = 0.0 # degree of overlap
-
-		for iObject in self.objects[1:]:
-			amountOfOverlap = Distributed.distance(iObject.sdr, sdr)
-
-			# TODO< treat as no overlap if the number of overlapping bits is to small >
-			## we need to do this because it lowers the probability that the reasoning process
-			## derives non-sense just because one or to many bits were identically by chance
-
-			if amountOfOverlap > bestObjectOverlap:
-				bestObjectOverlap = amountOfOverlap
-				bestObject = iObject
-
-
-		if bestObjectOverlap < self.retrieveBeliefMinimalOverlap:
-			return None
-
-		return bestObject
 
 
 
 
 
 import NarsInference
+from NarsMemory import NarsMemory
 from TruthValue import TruthValue
 from Task import Task
 from Stamp import Stamp
