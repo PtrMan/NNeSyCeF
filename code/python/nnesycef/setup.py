4231f1f5650b160aae3e11afe35ba4f40f7b0231
PtrMan <robertw89@googlemail.com>
2018-10-02 05:54:50 +0200

    Opt: cytonized Reasoner

--- code/python/nnesycef/setup.py
+++ code/python/nnesycef/setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules = cythonize(["Distributed.pyx", "NarsMemory.pyx"])
+    ext_modules = cythonize(["Distributed.pyx", "NarsMemory.pyx", "Reasoner.pyx"])
 )
