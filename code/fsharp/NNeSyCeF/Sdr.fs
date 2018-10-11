d0f1d03ae0ddd1207c241aef2ec9ffe0c309ee3a
PtrMan <robertw89@googlemail.com>
2018-10-11 17:02:48 +0200

    Refactor: SDR

--- code/fsharp/NNeSyCeF/Sdr.fs
+++ code/fsharp/NNeSyCeF/Sdr.fs
@@ -7,19 +7,24 @@ module Sdr
 
   let SDR_SIZE = 2048
 
-  let sdrAnd(a:Sdr, b:Sdr) =
+  let and_ (a:Sdr) (b:Sdr) =
     Sdr [| for i in 0 .. ((Array.length a.arr) - 1) -> ((a.arr.[i]) && (b.arr.[i])) |] 
 
-  let sdrUnion a b =
-    [| for i in 0 .. ((List.length a) - 1) -> ((List.nth a i) || (List.nth b i)) |] 
+  let union (a:Sdr) (b:Sdr) =
+    Sdr [| for i in 0 .. ((Array.length a.arr) - 1) -> (a.arr.[i] || b.arr.[i]) |] 
+  
+  let overlapBitcount (a:Sdr) (b:Sdr) =
+    // we need to count the number of overlapping bits of both
+    let commonSdr = and_ a b
+    let countOfOverlappingBits = (Array.filter (fun x -> x) commonSdr.arr) |> Array.length
+    countOfOverlappingBits
 
-  let sdrZero = Sdr(Array.zeroCreate SDR_SIZE)
+  let zero = Sdr(Array.create SDR_SIZE false)
 
   // checks a for complete overlap with b
-  let sdrCheckOverlap(a:Sdr, b:Sdr) =
+  let checkOverlap (a:Sdr) (b:Sdr) =
     // we need to count the number of overlapping bits of both
-    let commonSdr = sdrAnd(a, b)
-    let countOfOverlappingBits = (Array.filter (fun x -> x) commonSdr.arr) |> Array.length
+    let countOfOverlappingBits = overlapBitcount a b
   
     let countOfBBits = b.arr |> Array.filter (fun x -> x) |> Array.length
   
